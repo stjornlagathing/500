5a0a48c32a1ae99bbbc1c33bcfe661db5f1d9663
Aitor García <aitor.garcia@gmail.com>
2010-11-10 11:38:42 +0000

    Show vote in the index

--- app/helpers/candidates_helper.rb
+++ app/helpers/candidates_helper.rb
@@ -1,2 +1,14 @@
 module CandidatesHelper
+  #def show_review(c)
+  #  return "Já" if is_aye(c)
+  #  return "Kannski" if is_maybe(c)
+  #  return "Nei" if is_nay(c)
+  #end
+  def show_review(c)
+    return "Já" if current_user.review_for(c).vote == Review::AYE
+    return "Kannski" if current_user.review_for(c).vote == Review::MAYBE
+    return "Nei" if current_user.review_for(c).vote == Review::NAY
+  end
 end
+
+
